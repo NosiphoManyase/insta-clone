72f17c8afa15205cc7428a19c34c31c6c91b79d5
Nosipho <manyasenosipho@gmail.com>
2023-03-17 07:31:10 +0200

    create post header & image

--- components/Posts.js
+++ components/Posts.js
@@ -6,19 +6,19 @@ export default function Posts() {
         {
             id: '1',
             username: 'ivyaligns',
-            userImg: 'https://i.pravatar.cc/150?img=35',
+            userImg: 'https://i.pravatar.cc/150?img=5',
             img: 'https://plus.unsplash.com/premium_photo-1666184129845-ddaf361f9410?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
             caption: 'hanging with the girlies'
         },
         {
             id: '2',
             username: 'camBam',
-            userImg: 'https://i.pravatar.cc/150?img=5',
+            userImg: 'https://i.pravatar.cc/150?img=18',
             img: 'https://images.unsplash.com/photo-1678933632108-0ea1b61b2082?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
             caption: 'travel life'
         },
         {
-            id: '1',
+            id: '3',
             username: 'Michael_Kool',
             userImg: 'https://i.pravatar.cc/150?img=12',
             img: 'https://plus.unsplash.com/premium_photo-1666184129845-ddaf361f9410?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
@@ -32,8 +32,8 @@ export default function Posts() {
             <Post key={post.id} 
                 id={post.id}
                 username={post.username}
-                useImg={post.userImg}
-                img={post.userImg}
+                userImg={post.userImg}
+                img={post.img}
                 caption={post.caption}
             />
         ))}
