269585f3aaa44250ebd4917cc81b966a7f9088d9
Nosipho <manyasenosipho@gmail.com>
2023-03-23 12:40:43 +0200

    use Session to wire miniprofile and user story

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,15 +1,17 @@
-
+import { useSession, signOut } from "next-auth/react"
 
 function MiniProfile() {
+    const {data: session} = useSession()
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
-        <img src='https://pyxis.nymag.com/v1/imgs/7b3/8ee/2f39dd057938055919d3822868da282c5f-halle-bailey.rsquare.h600.jpg'
+        <img src={session?.user.image}
          alt='user image' className="h-16 rounded-full border p-[2px]"/>
          <div className="flex-1 ml-4">
-            <h2 className="font-bold">RealHalleBailey</h2>
+            <h2 className="font-bold">{session?.user.username}</h2>
             <h3 className="text-sm text-gray-400">Welcome to instagram</h3>
          </div>
-        <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+        <button onClick={signOut} className="font-semibold text-blue-400 text-sm">Sign out</button>
     </div>
   )
 }
--- components/Story.js
+++ components/Story.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { PlusIcon } from '@heroicons/react/24/outline'
 
-export default function Story({username, img, id}) {
+export default function Story({username, img, id, isUser}) {
   return (
-    <div key={id}>
+    <div key={id} className='relative group cursor pointer'>
         <img src={img} alt={username}
         className='h-14 rounded-full p-[1..5px] border-red-500 border-2
-        cursor-pointer hover:scale-110 transition-transform duration-200
+        cursor-pointer group:hover:scale-110 transition-transform duration-200
         ease-out' />
+        {isUser && <PlusIcon className='h-6 absolute font-bold  top-4 left-4 text-white' />}
         <p className='text-xs w-14 truncate'>{username}</p>
     </div>
   )
