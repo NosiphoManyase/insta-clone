f31b5834967adb5bb60469cd2055c0da2818166b
Nosipho <manyasenosipho@gmail.com>
2023-03-27 12:02:18 +0200

    show comments from firebase db in comment section

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { EllipsisHorizontalIcon, HeartIcon, ChatBubbleOvalLeftEllipsisIcon, BookmarkIcon, FaceSmileIcon } from '@heroicons/react/24/outline'
 import { useSession } from 'next-auth/react'
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase'
+import Moment from 'react-moment';
 
 export default function Post({ img, userImg, caption, username, id}) {
     const { data: session } = useSession()
     const [comment, setComment] = useState('')
+    const [comments, setComments] = useState('')
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+        query(
+            collection(db, 'posts', id, 'comments'),
+            orderBy('timestamp', 'desc')), 
+            snapshot => setComments(snapshot.docs)
+        )
+    }, [db, id])
 
     const sendComment = async (e) => {
         e.preventDefault()
@@ -50,6 +61,19 @@ export default function Post({ img, userImg, caption, username, id}) {
         <p className='p-5 truncate'>
             <span className='font-bold mr-2'>{username}</span>
         {caption}</p>
+        {comments.length > 0 && (
+            <div className='mx-10 max-h-24 overflow-y-scroll scrollbar-none'>
+                {comments.map(comment =>(
+                    <div className='flex items-center space-x-2 mb-2'>
+                        <img src={comment.data().userImage} alt='user image'
+                        className='h-7 rounded-full object-cover'/>
+                        <p className='font-semibold'>{comment.data().username}</p>
+                        <p className='flex-1 truncate'>{comment.data().comment}</p>
+                        <Moment fromNow date={comment.data().timestamp?.toDate()} />
+                    </div>
+                ))}
+            </div>
+        )}
 
         {/* Post Comment input */}
         {session && (
